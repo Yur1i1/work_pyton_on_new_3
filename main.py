d42e43292f1217f22b1574243c6a8b68c05b80ea
Yur1i1 <93601581+Yur1i1@users.noreply.github.com>
2021-11-16 17:56:23 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+#Задание_4
+
+import numpy as np
+
+x = np.array(range(100))
+print(np.mean(x))
+
+#Задание_5
+
+v = np.array([10, 20, 30, 40])
+d = np.arange(4)
+print(v + d)
+
+
+#Задание_6
+
+a = int(1)
+b = int (100)
+c = (sum(range(a+b))) /b
+print (c)
+
+
+
+
+
+
